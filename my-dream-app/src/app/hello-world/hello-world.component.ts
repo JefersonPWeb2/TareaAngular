90c92b44dfcbda94a62b3d77d87cb3ea807552bc
Jeferson <jbasurco@unsa.edu.pe>
2024-07-17 17:44:45 -0500

    Implementando OnInit y creando componente User

--- my-dream-app/src/app/hello-world/hello-world.component.ts
+++ my-dream-app/src/app/hello-world/hello-world.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
   templateUrl: './hello-world.component.html',
   styleUrl: './hello-world.component.css'
 })
-export class HelloWorldComponent {
-
+export class HelloWorldComponent implements OnInit{
+  constructor() { }
+  ngOnInit(): void {
+  } 
 }
