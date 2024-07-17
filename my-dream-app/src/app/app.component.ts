dcb8ffcff9e78785d01d039addc47538bb5ff2e1
Jeferson <jbasurco@unsa.edu.pe>
2024-07-17 17:39:31 -0500

    Creación de un botón para mostrar hobbies y un formulario para añadir más, creación de un componente

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-dream-app';
-  name = "Jeferson Basurco C.";
-  email = "jbasurco@unsa.edu.pe";
-  webpage = "http://www.unsa.edu.pe";
+  name : string;
+  email; //: string;
+  webpage : string;
+  hobbies : string[];
+  showHobbies : boolean;
+  constructor(){
+    console.log("Constructor working...");
+    this.name = "Jeferson Joao";
+    this.email = "jbasurco@unsa.edu.pe";
+    this.webpage = "http://www.unsa.edu.pe";
+    this.hobbies = ["Futbol","Programación","Netflix"];
+    this.showHobbies = false;
+  }
+  showhobbies (){
+    return true;
+  }
+  toggleHobbies(){
+    this.showHobbies = !this.showHobbies;
+  }
+  newHobby(hobby:any){
+    //console.log(hobby.value);
+    this.hobbies.push(hobby.value);
+    hobby.value="";
+    return false;
+  }
 }
