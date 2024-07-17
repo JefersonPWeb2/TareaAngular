378212211f36ac4ac5c275a96720738b9eba47ff
Jeferson <jbasurco@unsa.edu.pe>
2024-07-17 16:48:30 -0500

    Vericando archivos

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  name = "Jeferson Basurco C.";
+  email = "jbasurco@unsa.edu.pe";
+  webpage = "http://www.unsa.edu.pe";
 }
