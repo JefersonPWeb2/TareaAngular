27b3a61fc6f89bf13b9868914327c63a2f2a54a2
Jeferson <jbasurco@unsa.edu.pe>
2024-07-17 18:53:45 -0500

    Haciendo DataBinding con datos del formulario

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
-  name : string;
+  name : string = "Jeferson Joao";
+  age : number = 19;
   email; //: string;
   webpage : string;
   hobbies : string[];
