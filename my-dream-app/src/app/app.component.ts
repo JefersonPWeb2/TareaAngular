7801211c52ca49b8ce68ca7e157e34e8d318a958
Jeferson <jbasurco@unsa.edu.pe>
2024-07-17 18:42:23 -0500

    Agregando formularios y botones para borrar usuarios

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -24,6 +24,20 @@ export class AppComponent {
   sayHello() {
     alert("Hola Desde app.component");
   }
+  deleteUser(user:any){
+    for (let i=0; i<this.users.length; i++){
+      if (user==this.users[i]){
+        this.users.splice(i,1);
+      }
+    }
+  }
+  addUser(newUser:any){
+    //console.log(newUser.value);
+    this.users.push(newUser.value);
+    newUser.value='';
+    newUser.focus();
+    return false;
+  }
   showhobbies (){
     return true;
   }
