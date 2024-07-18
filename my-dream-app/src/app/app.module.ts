cac7b83f1e3c581643b9a24f36e39b1c798eef04
Jeferson <jbasurco@unsa.edu.pe>
2024-07-17 19:36:18 -0500

    Insertando ruteos about y hello al proyecto

--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -1,24 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Route } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
 import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+
+const routes: Route[] = [
+  {path: '', component: AppComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'hello', component: HelloWorldComponent},
+]
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [
     provideClientHydration(),
